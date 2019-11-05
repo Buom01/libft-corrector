ee10de8dcca1558becb1000565de96f8480b5b7a
Bastien ADAM <badam@e1r13p5.42.fr>
2019-11-05 22:12:45 +0100

    Basic tests

---
+++ ft_atoi_test.c.sh
@@ -0,0 +1,25 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_atoi_test.c.sh                                  :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: badam <marvin@42.fr>                       +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2019/11/05 22:12:05 by badam             #+#    #+#              #
+#    Updated: 2019/11/05 22:12:17 by badam            ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#/bin/sh
+./ft_atoi_test.c.out "0" && OK || KO
+./ft_atoi_test.c.out "---0" && OK || KO
+./ft_atoi_test.c.out "1" && OK || KO
+./ft_atoi_test.c.out "-1" && OK || KO
+./ft_atoi_test.c.out "++-5" && OK || KO
+./ft_atoi_test.c.out "-5" && OK || KO
+./ft_atoi_test.c.out "9" && OK || KO
+./ft_atoi_test.c.out "-9" && OK || KO
+./ft_atoi_test.c.out "10" && OK || KO
+./ft_atoi_test.c.out "-10" && OK || KO
+./ft_atoi_test.c.out "    99" && OK || KO
+./ft_atoi_test.c.out "   -99" && OK || KO
---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+TESTFILES=*_test*.c
+export SUCCESS="\033[0;32m✔"
+export FAIL="\033[0;31m✘"
+export RESET="\033[0m"
+
+OK()
+{
+	printf "$SUCCESS$RESET"
+}
+KO()
+{
+	printf "$FAIL$RESET"
+}
+
+export -f OK
+export -f KO
+
+rm -Rf *.out
+
+for test in $TESTFILES
+do
+	printf "$test : "
+	gcc -Wall -Wextra -Werror -I../libft/ $test -o $test.out
+	if [[ -f $test.sh ]]; then
+		./$test.sh
+	else
+		./$test.out && OK || KO
+	fi
+	printf "\n"
+done
