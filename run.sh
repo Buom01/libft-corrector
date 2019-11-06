4202455524248b67287d8a4596e73e23ffc4edce
Bastien ADAM <badam@e2r4p23.42.fr>
2019-11-06 21:15:20 +0100

    Working tests

--- run.sh
+++ run.sh
@@ -10,11 +10,11 @@ export RESET="\033[0m"
 
 OK()
 {
-	printf "$SUCCESS$RESET"
+	printf "$SUCCESS $RESET"
 }
 KO()
 {
-	printf "$FAIL$RESET"
+	printf "$FAIL $RESET"
 }
 
 export -f OK
@@ -23,8 +23,10 @@ export -f KO
 rm -Rf *.out
 
 printf "Compilation : "
-sh -c "cd $LIBFT_PATH && make re &>/dev/null" && OK || (KO && exit)
-sh -c "cd $LIBFT_PATH && make bonus &>/dev/null" && OK || (KO && exit)
+sh -c "cd $LIBFT_PATH && make re &>/dev/null" && OK && printf "(Project)  " \
+		|| (KO && exit)
+sh -c "cd $LIBFT_PATH && make bonus &>/dev/null" && OK && printf "(Bonus)" \
+		|| (KO && exit)
 printf "\n"
 
 for test in $TESTFILES
@@ -33,7 +35,7 @@ do
 	sh -c "gcc -Wall -Wextra -Werror -I $LIBFT_PATH $test $LIBFT_PATH*.o -o $test.out" &>/dev/null
 	if [[ -f $test.out ]]; then
 		if [[ -f $test.sh ]]; then
-			./$test.sh
+			./$test.sh 2> /dev/null  # Move out errors
 			printf "  \033[0;90m(using -.sh)$RESET"
 		else
 			./$test.out &>/dev/null && OK || KO
