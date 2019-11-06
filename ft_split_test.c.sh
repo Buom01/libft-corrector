ed9274cda824b21e89391dd9199594ce45480f80
Bastien ADAM <badam@e2r4p23.42.fr>
2019-11-06 21:21:10 +0100

    Hardening tests

--- ft_split_test.c.sh
+++ ft_split_test.c.sh
@@ -6,7 +6,7 @@
 #    By: badam <marvin@42.fr>                       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/11/05 22:12:05 by badam             #+#    #+#              #
-#    Updated: 2019/11/06 16:28:29 by badam            ###   ########.fr        #
+#    Updated: 2019/11/06 21:20:41 by badam            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,6 +19,9 @@
 ./ft_split_test.c.out "qweerty" "e" "qw" "rty" && OK || KO
 ./ft_split_test.c.out "erty" "e" "rty" && OK || KO
 ./ft_split_test.c.out "qwe" "e" "qw" && OK || KO
+./ft_split_test.c.out "eeet" "e" "t" && OK || KO
+./ft_split_test.c.out "teee" "e" "t" && OK || KO
+./ft_split_test.c.out "eeeteee" "e" "t" && OK || KO
 ./ft_split_test.c.out "eee" "e" && OK || KO
 ./ft_split_test.c.out "ee" "e" && OK || KO
 ./ft_split_test.c.out "e" "e" && OK || KO
