304b3fecb7413766098ab730066d7f2ba00be91e
Bastien ADAM <badam@e1r1p4.42.fr>
2019-11-09 17:50:47 +0100

    Test tests

--- ft_memccpy_test.c
+++ ft_memccpy_test.c
@@ -6,7 +6,7 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 21:34:43 by badam             #+#    #+#             */
-/*   Updated: 2019/11/08 18:59:03 by badam            ###   ########.fr       */
+/*   Updated: 2019/11/09 17:50:20 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,18 +28,19 @@ int	main(int argc, char **argv)
 
 	if (argc < 4)
 		return (1);
-	memset(memdst, argv[1][0], MEMLEN );
-	memset(ft_memdst, argv[1][0], MEMLEN );
+	memset(memdst, argv[1][0], MEMLEN - 1);
+	memset(ft_memdst, argv[1][0], MEMLEN - 1);
+	memset(memdst + MEMLEN - 1, '\0', 1);
+	memset(ft_memdst + MEMLEN - 1, '\0', 1);
 	c = (int)(argv[3][0]);
 	n = atoi(argv[4]);
 	rslt = memccpy(memdst, argv[2], c, n);
 	ft_rslt = ft_memccpy(ft_memdst, argv[2], c, n);
-	printf("\n%s:%s ; %i:%i", ft_memdst, memdst,
-		(int)(ft_rslt - (void*)ft_memdst), (int)(rslt - (void*)memdst));
+	//printf("\n%s:%s ; %i", ft_memdst, memdst,
+	//		(ft_rslt - (void*)ft_memdst) != (rslt - (void*)memdst));
 	if (memcmp(ft_memdst, memdst, MEMLEN) != 0
-			|| ((rslt == NULL && ft_rslt != NULL)
-				&& (rslt != NULL && ft_rslt == NULL)
-				&& (ft_rslt - (void*)ft_memdst) != (rslt - (void*)memdst))
+			|| (!(rslt == NULL && ft_rslt == NULL)
+				&& (ft_rslt - (void*)ft_memdst) != (rslt - (void*)memdst)))
 		return (1);
 	return (0);
 }
