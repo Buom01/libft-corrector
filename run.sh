5d74912d76e634d4428de92baeab07aad8c786ed
Bastien ADAM <badam@e1r1p4.42.fr>
2019-11-10 19:41:08 +0100

    Revert run.sh

--- run.sh
+++ run.sh
@@ -38,10 +38,10 @@ printf "\n\n"
 for test in $TESTFILES
 do
 	printf "$test : "
-	sh -c "clang -Wall -Wextra -Werror -g3 -fsanitize=address -I $LIBFT_PATH $test $LIBFT_PATH*.o -o $test.out" #&>/dev/null
+	sh -c "clang -Wall -Wextra -Werror -g3 -fsanitize=address -I $LIBFT_PATH $test $LIBFT_PATH*.o -o $test.out" &>/dev/null
 	if [[ -f $test.out ]]; then
 		if [[ -f $test.sh ]]; then
-			./$test.sh #2> /dev/null  # Move out errors
+			./$test.sh 2> /dev/null  # Move out errors
 			printf "  \033[0;90m(using -.sh)$RESET"
 		else
 			./$test.out &> /dev/null && OK || KO
