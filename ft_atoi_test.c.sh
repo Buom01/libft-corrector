6f63c09bcf8acd7a55f7ecae1772c6cdb4db0401
Bastien ADAM <badam@e1r2p4.42.fr>
2019-11-12 20:19:53 +0100

    Update test

--- ft_atoi_test.c.sh
+++ ft_atoi_test.c.sh
@@ -6,7 +6,7 @@
 #    By: badam <marvin@42.fr>                       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/11/05 22:12:05 by badam             #+#    #+#              #
-#    Updated: 2019/11/11 21:11:46 by badam            ###   ########.fr        #
+#    Updated: 2019/11/12 20:14:23 by badam            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -26,3 +26,6 @@
 ./ft_atoi_test.c.out "" && OK || KO
 ./ft_atoi_test.c.out "+2147483647 (max int val)" && OK || KO
 ./ft_atoi_test.c.out "-2147483648" && OK || KO
+SEPARATE
+./ft_atoi_test.c.out "99999999999999999999999999" && OK || KO
+./ft_atoi_test.c.out "-9999999999999999999999999" && OK || KO
